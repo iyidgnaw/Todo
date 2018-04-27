6dd74dad6f824a3c66ec31f5224acd9bbfd79b55
WangDiyi <wang.di@husky.neu.edu>
2018-04-27 10:57:12 -0400

    0.1 Version

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-#! /usr/bin/env python
-__author__ = 'Diyi Wang'
+#! /usr/local/bin/python3
 '''Main module of my Todo app'''
 
 import argparse
 import os
 import pickle
-import shlex
-CACHE = '/Users/diyi.wang/Github/Python/Todo/appCache'
 
-class toDoApp(object):
+CACHE = os.getcwd() + '/appCache'
+
+class TodoApp(object):
     '''todo application main class'''
     def __init__(self):
         self._registered = {'Default': toDoList()}
@@ -30,15 +29,15 @@ class toDoApp(object):
     def showAllThing(self):
         print('Current List: {}'.format(self._using))
         currentList = self._registered[self._using]
-        currentList.showAll()
+        currentList.printList()
 
     def showAllList(self):
         for name in self._registered:
             print(name)
 
-    def deleteThing(self, id):
+    def deleteThing(self, t_id):
         currentList = self._getCurrentList()
-        currentList.deleteThing(id)
+        currentList.deleteThing(t_id)
 
     def deleteList(self, nameOfList):
         self._registered.pop(nameOfList, None)
@@ -72,79 +71,54 @@ class toDoList(object):
         newThing = toDoThing(content)
         self._listOfThings.append(newThing)
 
-    def deleteThing(self, id):
-        self._listOfThings.pop(int(id)-1)
+    def deleteThing(self, t_id):
+        self._listOfThings.pop(int(t_id)-1)
 
-    def showAll(self):
-        for id, thing in enumerate(self._listOfThings):
-            print("{}. {}".format(str(id+1), thing.getContent()))
+    def printList(self):
+        for t_id, thing in enumerate(self._listOfThings):
+            print('{}. {}'.format(str(t_id+1), thing.getContent()))
 
 
 if __name__ == '__main__':
-    print ("TODO is Todo!")
-    supportCmd = {'show': 'showAllThing' , # show things in current list
-                  'listall': 'showAllList', # list all lists
-                  'add': 'addThing', # Add thing
-                  'delete': 'deleteThing', # Delete thing
-                  'abort': 'deleteList', # Delete list
-                  'new': 'createList', # Create new list
+    print ('#'*50)
+    print ('TODO is Todo!')
+    print ('Author: iyidgnaw')
+    print ('#'*50)
+    supportCmd = {'show': 'showAllThing',# show things in current list
+                  'listall': 'showAllList',# list all lists
+                  'add': 'addThing',# Add thing
+                  'delete': 'deleteThing',# Delete thing
+                  'abort': 'deleteList',# Delete list
+                  'new': 'createList',# Create new list
                   'use': 'useList' # Switch to list
                   }
     if not os.path.isfile(CACHE):
         # Init the app
-        todo = toDoApp()
-        print ("Init process done!")
+        todo = TodoApp()
+        print ('Init process done!')
     else:
         with open(CACHE, 'rb') as f:
             todo = pickle.load(f)
 
-    
     parser = argparse.ArgumentParser(description='Todo List App in terminal')
     parser.add_argument('command', choices=supportCmd.keys())
-    parser.add_argument('-n', '--name', type=str, 
+    parser.add_argument('-n', '--name', type=str,
         help='The given list name', default=None)
     parser.add_argument('-c', '--content', type=str,
         help='The content you want in the Thing', default=None)
     parser.add_argument('--id', type=int,
         help='The id you want to delete', default=None)
     args = parser.parse_args()
-    
+
     func = getattr(todo, supportCmd[args.command])
-    # if args.command == 'add':
-    #     func(args.content)
-    # elif args.command in ['use', 'abort', 'new']:
-    #     func(args.name)
-    # elif args.command == 'delete':
-    #     func(args.id)
-    # else:
-    #     func()
-    func(args)
-    todo.showAllThing()
+    if args.command == 'add':
+        func(args.content)
+    elif args.command in ['use', 'abort', 'new']:
+        func(args.name)
+    elif args.command == 'delete':
+        func(args.id)
+    else:
+        func()
 
     with open(CACHE, 'wb') as f:
         pickle.dump(todo, f)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
